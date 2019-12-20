29b0a35c49a6d28f25ebc8351720f782c2666284
Odunsi Joseph <odunsiolakunbi@gmail.com>
2019-12-20 16:01:25 +0100

    added localstorage feature

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-// Library array to store the books
-let myLibrary = [
-    new Book("The Hobbit", "J.R.R. Tolkien", 295, true)
-];
+let myLibrary = storeBooks()
 
 // function constructor for the book
 function Book(title, author, pages, read = true) {
@@ -19,6 +16,7 @@ function addBookToLibrary() {
     let read = document.querySelector('[data-name=read]').checked;
 
     myLibrary.push(new Book(title, author, pages, read))
+    localStorage.setItem('library', JSON.stringify(myLibrary))
 }
 
 // clear field after submitting a new book
@@ -32,32 +30,35 @@ function clearFields() {
 // add the book details to the DOM
 function render() {
     const list = document.querySelector('.bookList');
-    const row = document.createElement('tr');
-    myLibrary.forEach(books => {
+    let row;
+    for (var book of myLibrary) {
+        row = document.createElement('tr')
         row.innerHTML =
             `
-        <td>${books.title}</td>
-        <td>${books.author}</td>
-        <td>${books.pages}</td>
-        <td class="read-status">${books.read}</td>
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.pages}</td>
+        <td class="read-status">${book.read}</td>
         <td><button class="delete">Delete</button></td>
         `
-    });
-    list.appendChild(row)
+        list.appendChild(row)
+    }
 }
 
 // event listener to display book details on add button
 document.querySelector('form').addEventListener('submit', (e) => {
-    let title = document.querySelector('[data-name=title]').value;
-    let author = document.querySelector('[data-name=author]').value;
-    let pages = document.querySelector('[data-name=page]').value;
+    const title = document.querySelector('[data-name=title]').value;
+    const author = document.querySelector('[data-name=author]').value;
+    const pages = document.querySelector('[data-name=page]').value;
 
     if (title == "" || author == "" || pages == "") {
         alert('Fill in the empty spaces')
     } else {
+        document.querySelector('.bookList').innerHTML = ''
         addBookToLibrary();
         render();
         clearFields();
+        document.querySelector("[data-name=title]").focus();
     }
     e.preventDefault();
 });
@@ -66,10 +67,11 @@ document.querySelector('form').addEventListener('submit', (e) => {
 document.querySelector('.bookList').addEventListener('click', (e) => {
     if (e.target.classList.contains('delete')) {
         e.target.parentElement.parentElement.remove();
+        removebook(e.target.parentElement.parentElement.children[0].textContent)
     }
 })
 
-//load default book details upon opening the page
+//load existing books upon opening the page
 document.addEventListener('DOMContentLoaded', render);
 
 // show add book form
@@ -78,27 +80,28 @@ let openForm = () => {
         let form = document.querySelector('form');
         form.style.display = 'block'
         document.querySelector('.add').style.display = 'none';
+        document.querySelector('[data-name=title]').focus()
     })
 }
 
 // close add book form
-let closeForm = ()=>{
-    document.querySelector('.fa-times').addEventListener("click", ()=>{
+let closeForm = () => {
+    document.querySelector('.fa-times').addEventListener("click", () => {
         let form = document.querySelector('form');
         form.style.display = 'none'
         document.querySelector('.add').style.display = 'block';
     })
-} 
+}
 
 // change status of read
-let changeRead = () =>{
-    document.querySelector('.bookList').addEventListener('click', (e)=>{
-        if(e.target.className == "read-status"){
-         if(e.target.textContent == '✔'){
-            e.target.textContent = "❌"
-         } else{
-             e.target.textContent = "✔"
-         }
+let changeRead = () => {
+    document.querySelector('.bookList').addEventListener('click', (e) => {
+        if (e.target.className == "read-status") {
+            if (e.target.textContent == '✔') {
+                e.target.textContent = "❌"
+            } else {
+                e.target.textContent = "✔"
+            }
         }
     })
 }
@@ -106,4 +109,29 @@ let changeRead = () =>{
 openForm()
 closeForm()
 changeRead()
+
+
+function storeBooks() {
+    let books;
+    if (localStorage.getItem('library') === null) {
+        books = []
+    } else {
+        books = JSON.parse(localStorage.getItem('library'))
+    }
+    return books
+}
+
+// remove book
+function removebook(title) {
+    const book = document.querySelector('.bookList');
+    const library = JSON.parse(localStorage.getItem('library'))
+
+    library.forEach((book, index) => {
+        // console.log(index, book.title)
+        if (book.title == title) {
+            library.splice(index, 1)
+        }
+        localStorage.setItem('library', JSON.stringify(library))
+    })
+}
 
